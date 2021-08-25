a76ccf1b9b92d9145af7fc35f103992fe652e416
kimdoori <dooooreeee@naver.com>
2021-08-25 19:28:58 +0900

    도메인 분석 설계

--- SpringJPA/jpashop/src/main/java/jpabook/HelloController.java
+++ SpringJPA/jpashop/src/main/java/jpabook/HelloController.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop;
+package jpabook;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- SpringJPA/jpashop/src/main/java/jpabook/JpashopApplication.java
+++ SpringJPA/jpashop/src/main/java/jpabook/JpashopApplication.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop;
+package jpabook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 	
 	public static void main(String[] args) {
-		
-		Hello hello=new Hello();
-		hello.setData("asd");
-		String data = hello.getData();
-		System.out.println("data = " + data);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
