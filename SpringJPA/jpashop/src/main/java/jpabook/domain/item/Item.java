0aabb1349a84f3e6a438bd6df889a5bedfbb776d
kimdoori <dooooreeee@naver.com>
2021-08-26 19:08:08 +0900

    상품 도메인

--- SpringJPA/jpashop/src/main/java/jpabook/domain/item/Item.java
+++ SpringJPA/jpashop/src/main/java/jpabook/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.domain.item;
 
 import jpabook.domain.Category;
+import jpabook.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories=new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * 재고수량 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 }
